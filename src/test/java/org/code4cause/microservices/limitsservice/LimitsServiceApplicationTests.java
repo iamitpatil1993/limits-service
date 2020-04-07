16e1427188bfd0f8acc7dc01a6386accad5518d4
amipatil <iamitpatil1993@gmail.com>
2020-04-08 00:56:04 +0530

    Initial Commit

---
+++ src/test/java/org/code4cause/microservices/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.code4cause.microservices.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
